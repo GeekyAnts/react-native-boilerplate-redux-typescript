c9c428a679f876ed9050aabdb11843cdb9ca5b73
Shivraj <shivraj@geekyants.com>
2017-09-27 15:27:04 +0530

    eslint fix

--- src/stories/screens/Login/index.tsx
+++ src/stories/screens/Login/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Image, Platform } from "react-native";
+import { Image } from "react-native";
 import { Container, Content, Header, Body, Title, Button, Text, View, Icon, Footer } from "native-base";
 //import styles from "./styles";
 export interface Props {
