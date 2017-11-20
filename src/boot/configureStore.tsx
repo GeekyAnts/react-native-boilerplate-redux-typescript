a7ed13a73b06b5b13c03ad412b6a8029a308f39c
Shivraj <shivraj@geekyants.com>
2017-11-20 11:22:52 +0530

    dependencies upgrade

--- src/boot/configureStore.tsx
+++ src/boot/configureStore.tsx
@@ -1,4 +1,3 @@
-import { AsyncStorage } from "react-native";
 import devTools from "remote-redux-devtools";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
@@ -6,16 +5,16 @@ import { persistStore } from "redux-persist";
 import reducer from "../reducers";
 
 export default function configureStore(onCompletion: () => void): any {
-	const enhancer = compose(
-		applyMiddleware(thunk),
-		devTools({
-			name: "nativestarterkit",
-			realtime: true,
-		})
-	);
+  const enhancer = compose(
+    applyMiddleware(thunk),
+    devTools({
+      name: "nativestarterkit",
+      realtime: true
+    })
+  );
 
-	const store = createStore(reducer, enhancer);
-	persistStore(store, { storage: AsyncStorage }, onCompletion);
+  const store = createStore(reducer, enhancer);
+  persistStore(store, onCompletion);
 
-	return store;
+  return store;
 }
--- src/boot/setup.tsx
+++ src/boot/setup.tsx
@@ -9,42 +9,42 @@ import getTheme from "../theme/components";
 import variables from "../theme/variables/platform";
 export interface Props {}
 export interface State {
-	store: Object;
-	isLoading: boolean;
-	isReady: boolean;
+  store: Object;
+  isLoading: boolean;
+  isReady: boolean;
 }
 export default class Setup extends React.Component<Props, State> {
-	constructor() {
-		super();
-		this.state = {
-			isLoading: false,
-			store: configureStore(() => this.setState({ isLoading: false })),
-			isReady: false,
-		};
-	}
-	componentWillMount() {
-		this.loadFonts();
-	}
-	async loadFonts() {
-		await Expo.Font.loadAsync({
-			Roboto: require("native-base/Fonts/Roboto.ttf"),
-			Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-			Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-		});
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      store: configureStore(() => this.setState({ isLoading: false })),
+      isReady: false
+    };
+  }
+  componentWillMount() {
+    this.loadFonts();
+  }
+  async loadFonts() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
 
-		this.setState({ isReady: true });
-	}
+    this.setState({ isReady: true });
+  }
 
-	render() {
-		if (!this.state.isReady || this.state.isLoading) {
-			return <Expo.AppLoading />;
-		}
-		return (
-			<StyleProvider style={getTheme(variables)}>
-				<Provider store={this.state.store}>
-					<App />
-				</Provider>
-			</StyleProvider>
-		);
-	}
+  render() {
+    if (!this.state.isReady || this.state.isLoading) {
+      return <Expo.AppLoading />;
+    }
+    return (
+      <StyleProvider style={getTheme(variables)}>
+        <Provider store={this.state.store}>
+          <App />
+        </Provider>
+      </StyleProvider>
+    );
+  }
 }
